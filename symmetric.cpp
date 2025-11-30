b712de07dd45d2620b01e3f7ea4940c6eb4e7ce8
zunie05 <zunairarashed2005@gmail.com>
2025-11-30 21:17:35 +0600

    Add files via upload

---
+++ symmetric.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,i,j,c;
+    cin>>m>>n;
+    int arr[m][n];
+    c=0;
+    if(m!=n)
+    {
+        cout<<"not symmetric";
+        return 0;
+    }
+
+
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(arr[i][j]==arr[j][i])
+            {
+            c=c+1;
+            }
+
+
+        }
+
+    }
+
+    if(c==m*m)
+    {
+        cout<<"symmetric"<<endl<<endl;
+     for(i=0;i<m;i++)
+       {
+        for(j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+
+       }
+    }
+    else
+    {
+        cout<<"not symmetric";
+    }
+
+    return 0;
+
+}
